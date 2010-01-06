1846d7d2c80d8c90ffe95d0f96f60594ab7cc49b
Jar.Patryk <Jar.Patryk@126654e2-f7be-11de-8591-91bc733d3be8>
2010-01-06 22:03:04 +0000

    refaktoryzacja: dodanie public Vector<Spot> getNeighbours() dodania theSameField dodanie xX = x - offsetX dodanie yY = y - offsetY

--- branch/ePilkarzyki/src/tiwjj/playground/Spot.java
+++ branch/ePilkarzyki/src/tiwjj/playground/Spot.java
@@ -10,6 +10,7 @@ package tiwjj.playground;
  * @author yarpo
  */
 import java.awt.Point;
+import java.util.Vector;
 
 public class Spot extends Point {
 
@@ -25,14 +26,39 @@ public class Spot extends Point {
      */
     static public Spot lastSpot;
 
+    private int xX;
+    private int yY;
+
     public Spot(int x, int y)
     {
         super(x, y);
+        this.xX = x - Size.OffsetX;
+        this.yY = y - Size.OffsetY;
     }
 
     public Spot(Point p)
     {
-        super(p.x, p.y);
+        this(p.x, p.y);
+    }
+
+    public double getXx()
+    {
+        return xX;
+    }
+
+    public double getYy()
+    {
+        return yY;
+    }
+
+    public boolean theSameField(int x, int y)
+    {
+        return (0 == this.distance(x, y));
+    }
+
+    public boolean theSameField(Point p)
+    {
+        return (0 == this.distance(p));
     }
 
     /**
@@ -59,4 +85,34 @@ public class Spot extends Point {
         return (this.distance(p) <= Size.MaxDistance);
     }
 
+    public Vector<Spot> getNeighbours()
+    {
+        Vector v = new Vector<Spot>();
+
+        int [] xs =  {
+                            this.x,                        // wspolrz x wyswietlana
+                            this.x - Size.HorizontalGap,   // na lewo
+                            this.x + Size.HorizontalGap    // na prawo
+                        };
+
+        int [] ys =  {
+                            this.y,    // wspolrzedna y wyswieltana
+                            this.y - Size.VerticalGap,   // do gory
+                            this.y + Size.VerticalGap    // na dol
+                        };
+
+        for(int i = 2; i >= 0; i--)
+        {
+            for(int j = 2; j >= 0; j--)
+            {
+                if (Playground.isSpotable(xs[i], ys[j]) &&
+                    !this.theSameField(xs[i], ys[j]))
+                {
+                    v.add(new Spot(xs[i], ys[j]));
+                }
+            }
+        }
+
+        return v;
+    }
 }
