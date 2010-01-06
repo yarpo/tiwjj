e53d27832d5d7a42a006ecba262535d9c37fb6e9
Jar.Patryk <Jar.Patryk@126654e2-f7be-11de-8591-91bc733d3be8>
2010-01-06 20:30:36 +0000

    refaktoryzacja - usuniecie distance (istnieje w klasie Point)

--- branch/ePilkarzyki/src/tiwjj/playground/Spot.java
+++ branch/ePilkarzyki/src/tiwjj/playground/Spot.java
@@ -35,41 +35,6 @@ public class Spot extends Point {
         super(p.x, p.y);
     }
 
-    public double distance(Point p)
-    {
-        int a = Math.abs(this.x-p.x);
-            a = a*a;
-        int b = Math.abs(this.y-p.y);
-            b = b*b;
-
-        return Math.sqrt((double)(a+b));
-    }
-
-    /**
-     Sprawdza czy podany jako parametr punkt powinien byc podswietlony
-     */
-    public boolean isHovered(Spot p)
-    {
-        return this.isHovered(p.x ,p.y);
-    }
-
-    public boolean isHovered(int x, int y)
-    {
-       if (    (this.x <= x + Size.HorizontalGap/2)
-                &&
-                (this.x >= x - Size.HorizontalGap/2)
-                &&
-                (this.y >= y - Size.VerticalGap/2)
-                &&
-                (this.y <= y + Size.VerticalGap/2)
-           )
-        {
-            return true;
-        }
-
-        return false;
-    }
-
     /**
      Sprawdza czy podany jako parametr
      */
