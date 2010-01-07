972ce30820bdb905f5189eeb7146b10eddae6b8e
Jar.Patryk <Jar.Patryk@126654e2-f7be-11de-8591-91bc733d3be8>
2010-01-07 20:38:01 +0000

    refaktoryzacja: dodanie theSameMove usuniecie int player - todo na przyszlosc

--- branch/ePilkarzyki/src/tiwjj/playground/Move.java
+++ branch/ePilkarzyki/src/tiwjj/playground/Move.java
@@ -1,12 +1,10 @@
 package tiwjj.playground;
-
 /**
  * Klasa Move
  * Pozwala na obsluge ruchow na Playground
  * @author  Patryk yarpo Jar
  * @date    6 - 01 - 2009
  */
-
 
 public class Move {
 
@@ -21,11 +19,11 @@ public class Move {
      */
     private Spot end;
 
+
     /**
      * Identyfikator druzyny
      */
     private int team;
-    // TODO: private int player;
 
 
     /**
@@ -60,11 +58,65 @@ public class Move {
         return this.end;
     }
 
+
     /**
      * getter identyfikatora teamu
      */
     public int getTeam()
     {
         return this.team;
+    }
+
+
+    /**
+     * Sprawdza czy dwa ruchy sa takie same
+     */
+    public static boolean theSameMove(Move m1, Move m2)
+    {
+        Spot m1_1 = m1.getStart();  // punkt startowy
+        Spot m1_2 = m1.getEnd();    // punkt koncowy rychu
+        Spot m2_1 = m2.getStart();  // punkt startowy
+        Spot m2_2 = m2.getEnd();    // punkt koncowy rychu
+
+        if ((m2_1.theSameField(m1_1) && m2_2.theSameField(m1_2))
+            ||
+            (m2_1.theSameField(m1_2) && m2_2.theSameField(m1_1)))
+        {
+            return true; // juz istnieje
+        }
+
+        return false;
+    }
+
+
+    /**
+     * Sprawdza czy aktualny ruch jest identyczny z podanym jako poarametr
+     *
+     * @param Move m
+     *
+     * @returns boolean
+     */
+    public boolean theSameMove(Move m)
+    {
+        return Move.theSameMove(this, m);
+    }
+
+
+    /**
+     * Sprawdza czy aktualny ruch jest taki sam jak podany jako porametr
+     * Wlaczajac to te sama druzyne
+     *
+     * @param Move m
+     * @param int team
+     *
+     * @returns boolean
+     */
+    public boolean theSameMove(Move m, int t)
+    {
+        if (m.getTeam() == this.team && Move.theSameMove(this, m))
+        {
+            return true;
+        }
+        return false;
     }
 }
