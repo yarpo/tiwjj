ca269e3904275a93f10800dd33b7ae8e58991575
Jar.Patryk <Jar.Patryk@126654e2-f7be-11de-8591-91bc733d3be8>
2010-01-07 00:23:33 +0000

    refaktoryzacja - komentarze

--- branch/ePilkarzyki/src/tiwjj/playground/Moves.java
+++ branch/ePilkarzyki/src/tiwjj/playground/Moves.java
@@ -1,61 +1,90 @@
-/*
- * Moves.java
- *
- * Klasa przechowujaca informacje o wszystkich ruchach wykonanych w grze
- * do tej pory
- */
-
 package tiwjj.playground;
 
 /**
- * @author  yarpo
- * @data    5 01 2010
+ * Klasa Moves
+ * Zbiornik na kolejne ruchy graczy
+ * @author  Patryk yarpo Jar
+ * @date    6 - 01 - 2009
  */
-
 import java.util.Vector;
 import java.util.Iterator;
 
 // TODO: zastanowic sie nad wprowadzeniem dodatkowo macierzy punktow
-// TODO: pokomentowac metody i atrybuty
 
 public class Moves {
 
+    /**
+     * wektor ruchow
+     */
     private Vector<Move> moves;
-    private Spot startPosition;
 
+
+    /**
+     * Konstruktor Moves 1
+     *
+     * Tworzy pierwszy ruch
+     *
+     * @param Move m
+     */
     public Moves(Move m)
     {
         this.moves = new Vector<Move>();
-        this.startPosition = m.getStart();
         this.moves.add(m);
     }
 
+
+    /**
+     * Konstruktor Moves 2
+     *
+     * Tworzy pierwszy ruch
+     *
+     * @param Spot s
+     * @param Spot e
+     * @param int team
+     */
     public Moves(Spot s, Spot e, int team)
     {
         this(new Move(s, e, team));
     }
 
+
+    /**
+     * Dodaje nowy ruch
+     *
+     * @param Move m
+     */
     public void add(Move m)
     {
         this.moves.add(m);
         Spot.lastSpot = m.getEnd();
     }
 
+// TODO: czy iterator jest szybszy od petli?
+    /**
+     * Zwraca iterator
+     * Dzieki temu mozna przechodzic po wszystkich ruchach
+     *
+     * @return Iterator
+     */
     public Iterator getIterator()
     {
         return this.moves.iterator();
     }
 
-    public Spot getLastPoint()
-    {
-        return this.moves.lastElement().getEnd();
-    }
-
+    /**
+     * Zwraca liczbe ruchow
+     *
+     * @returns int
+     */
     public int length()
     {
         return this.moves.size();
     }
 
+// TODO: uzyc Playground.isSpotable
+    /**
+     * Sprawdza, czy mozna przejsc na wskazane pole
+     */
     public boolean possible(Spot e)
     {
         // nie jest na polu
@@ -80,6 +109,10 @@ public class Moves {
         return this.isEmpty(e);
     }
 
+// TODO: przerobic na statyczna
+    /**
+     * Sprawdza czy ten ruch nie byl juz wykonany
+     */
     private boolean isEmpty(Spot e)
     {
         Iterator move = this.getIterator();
