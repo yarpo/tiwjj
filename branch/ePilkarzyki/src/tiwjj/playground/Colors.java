b972be3d03314ed1b0768ff5892b9b59ae709933
Jar.Patryk <Jar.Patryk@126654e2-f7be-11de-8591-91bc733d3be8>
2010-01-07 00:34:35 +0000

    zamiana z wartosci statycznych Color.black na tworzenie obiektow za pomoca operatora new

--- branch/ePilkarzyki/src/tiwjj/playground/Colors.java
+++ branch/ePilkarzyki/src/tiwjj/playground/Colors.java
@@ -13,9 +13,9 @@ public class Colors {
     public static final Color Hover  = new Color(100, 150, 50); // tla gdy mysz jest nad boiskiem
     public static final Color Lines  = new Color(140, 240, 180);// srodkowej lini
     public static final Color Points = new Color(100, 100, 100);// punktow nieaktywnych
-    public static final Color HoveredPoint = Color.blue;        // punktu podwietlonego
-    public static final Color FocusedPoint = Color.black;       // punktow osiagalnych z aktualnego
-    public static final Color CurrentPoint = Color.red;         // aktualny punkt koncowy
+    public static final Color HoveredPoint = new Color(0,0,255);        // punktu podwietlonego
+    public static final Color FocusedPoint = new Color(0,0,0);       // punktow osiagalnych z aktualnego
+    public static final Color CurrentPoint = new Color(255,0,0);         // aktualny punkt koncowy
     public static final Color Goals = new Color(255, 255, 255); // linie symulujace bramki
-    public static final Color [] Teams = {Color.black, Color.white}; // kolory druzyn
+    public static final Color [] Teams = {new Color(0,0,0), new Color(255,255,255)}; // kolory druzyn
 }
