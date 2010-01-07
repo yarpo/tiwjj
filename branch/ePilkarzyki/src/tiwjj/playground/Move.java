12f148c3e24ca536c0fc928998342edc501d4a5e
Jar.Patryk <Jar.Patryk@126654e2-f7be-11de-8591-91bc733d3be8>
2010-01-07 00:26:49 +0000

    refaktoryzacja - komentarze

--- branch/ePilkarzyki/src/tiwjj/playground/Move.java
+++ branch/ePilkarzyki/src/tiwjj/playground/Move.java
@@ -1,22 +1,40 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package tiwjj.playground;
 
 /**
- *
- * @author yarpo
+ * Klasa Move
+ * Pozwala na obsluge ruchow na Playground
+ * @author  Patryk yarpo Jar
+ * @date    6 - 01 - 2009
  */
+
 
 public class Move {
 
+    /**
+     * Punkt startowy ruchu
+     */
     private Spot start;
+
+
+    /** 
+     * Punkt koncowy ruchu
+     */
     private Spot end;
+
+    /**
+     * Identyfikator druzyny
+     */
     private int team;
     // TODO: private int player;
 
+
+    /**
+     * Konstruktor
+     *
+     * @param Spot s - punkt startowy
+     * @param Spot e - punkt koncowy
+     * @param int t - druzyna
+     */
     public Move(Spot s, Spot e, int t)
     {
         this.start = s;
@@ -24,16 +42,27 @@ public class Move {
         this.team = t;
     }
 
+
+    /**
+     * getter punktu startowego
+     */
     public Spot getStart()
     {
         return this.start;
     }
 
+
+    /**
+     * getter punktu koncowego 
+     */
     public Spot getEnd()
     {
         return this.end;
     }
 
+    /**
+     * getter identyfikatora teamu
+     */
     public int getTeam()
     {
         return this.team;
