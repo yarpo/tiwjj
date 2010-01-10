3c7324714cabcfdf5636e3f069c9ebda85a1453b
Jar.Patryk <Jar.Patryk@126654e2-f7be-11de-8591-91bc733d3be8>
2010-01-10 18:57:29 +0000

    Dodanie konstruktora pozwalajacego na wysylanie zdalnych komunikatow do serwera

--- branch/ePilkarzyki/src/tiwjj/actions/JoinGame.java
+++ branch/ePilkarzyki/src/tiwjj/actions/JoinGame.java
@@ -5,6 +5,8 @@
 
 package tiwjj.actions;
 
+import tiwjj.*;
+import tiwjj.communication.client.SecureClient;
 /**
  *
  * @author yarpo
@@ -12,6 +14,16 @@ package tiwjj.actions;
 import java.awt.event.*;
 
 public class JoinGame implements ActionListener  {
+
+    private Main.TEAM team;
+    private SecureClient client;
+
+    public JoinGame(SecureClient client, Main.TEAM team)
+    {
+        this.team = team;
+        this.client = client;
+    }
+
     public void actionPerformed(ActionEvent e)
     {
         javax.swing.JOptionPane.showMessageDialog(null, "alert " + e.getActionCommand() +"\n",
