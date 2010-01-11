69b2ac44592b5d24fc7d7128ad9c12d73e4a6f70
Jar.Patryk <Jar.Patryk@126654e2-f7be-11de-8591-91bc733d3be8>
2010-01-11 22:53:15 +0000

    protokół

--- branch/ePilkarzyki/src/tiwjj/communication/Exchanger.java
+++ branch/ePilkarzyki/src/tiwjj/communication/Exchanger.java
@@ -12,13 +12,18 @@ package tiwjj.communication;
 public class Exchanger implements java.io.Serializable {
 
     public enum MessagesType {
-        START,
-        NAME,
-        WINER,
-        LOSER,
-        NORMAL
+        CONNECT,        // C->S polaczenie z serwerem
+        OK,             // S->C serwer mowi klientowi, ze go przyjal
+        GO,             // S->C pozwala klientowi na ruch
+        CHECK_MOVE,     // C->S nowy ruch, sprawdzenie czy dobry
+        BAD_MOVE,       // S->C bledny ruch - sprobuj ponownie
+        UPDATE,         // S->C dobry ruch, update wektora ruchow, ale nadal gra przeciwnik
+        WINNER,         // S->C wygrales!
+        LOSER,          // S->C przegrales
+        END             // C->S walkower
     }
 
-    public String content;
     public MessagesType type;
+    public String content;
+    public int team;
 }
