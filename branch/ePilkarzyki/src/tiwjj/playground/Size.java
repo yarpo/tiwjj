fc61cf0c04dcf4b79549907291d42518abd6e0a5
Jar.Patryk <Jar.Patryk@126654e2-f7be-11de-8591-91bc733d3be8>
2010-01-17 18:34:37 +0000

    refaktoryzacja

--- branch/ePilkarzyki/src/tiwjj/playground/Size.java
+++ branch/ePilkarzyki/src/tiwjj/playground/Size.java
@@ -4,38 +4,130 @@ package tiwjj.playground;
  * Statyczna Klasa Size
  * Zawiera wielkosci boiska, odleglosci punktow, itp.
  * Zbiorczy kontener stalych liczbowych dla projektu
+ *
+ * Wielkosci wyrazone sa w px
+ *
  * @author  Patryk yarpo Jar
  * @date    6 - 01 - 2009
  */
 public class Size {
 
-    // wielkosci wyrazone sa w px
+    /**
+     * Szerokos jednego punktu
+     */
+    final static public int PointX  = 4;
 
-    final static public int PointX  = 4;                // szerokosc punktu
+
+    /**
+     * Wysokosc punktu
+     */
     final static public int PointY  = 4;                // wysokosc punktu
-    final static public int OffsetX = (int)(PointX/2);  // przesuniecie rysowanego punktu wzgledem jego wspolrzednej x
-    final static public int OffsetY = (int)(PointY/2);  // przesuniecie rysowanego punktu wzgledem wspolrzednej y
-    final static public int PointsX = 10;   // liczba punktow w pionie
-    final static public int PointsY = 12;   // liczba punktow w poziomie
-    final static public int StartXGrass   = 2;  // odleglosc murawy od gory canvasa
-    final static public int StartYGrass   = 10; // odleglosc murawy od bokow canvasa
-    final static public int VerticalGap   = 20; // odleglosc punktow miedzy soba w pionie
-    final static public int HorizontalGap = 20; // odleglosc punktow w poziomie
-    final static public int PlaygroundWidth  = HorizontalGap*PointsX;   // szerokosc boiska
-    final static public int PlaygroundHeight = VerticalGap*PointsY;     // wysokosc boiska
-    final static public int WIDTH   = PlaygroundWidth+StartXGrass*2;    // szerokosc calego apletu
-    final static public int HEIGHT  = PlaygroundHeight+StartYGrass*2;   // wysokosc apletu
-    final static public int GoalWidth  = 45;    // szerokosc bramki
-    final static public int GoalHeight = 5;     // wysokosc lini symulujacej bramke
-    final static public int HoverAreaX = 30;    // obszar podswietlany wokol aktualnego punktu w pionie
-    final static public int HoverAreaY = 30;    // obszar podswietlany wokol aktualnego punktu w poziomie
-    // maksymalny dystans miedzy dwoma punktami, ktore mialy tworzy jeden ruch
+
+
+    /**
+     * przesuniecie rysowanego punktu wzgledem jego wspolrzednej x
+     */
+    final static public int OffsetX = (int)(PointX/2);
+
+
+    /**
+     * przesuniecie rysowanego punktu wzgledem wspolrzednej y
+     */
+    final static public int OffsetY = (int)(PointY/2);
+
+
+    /**
+     * liczba punktow w pionie
+     */
+    final static public int PointsX = 10;
+
+
+    /**
+     * liczba punktow w poziomie
+     */
+    final static public int PointsY = 12;
+
+
+    /**
+     * odleglosc murawy od gory canvasa
+     */
+    final static public int StartXGrass   = 2;
+
+
+    /**
+     * odleglosc murawy od bokow canvasa
+     */
+    final static public int StartYGrass   = 10;
+    
+    
+    /**
+     *  odleglosc punktow miedzy soba w pionie
+     */
+    final static public int VerticalGap   = 20;
+
+
+    /**
+     * odleglosc punktow w poziomie
+     */
+    final static public int HorizontalGap = 20;
+
+
+    /**
+     * szerokosc boiska
+     */
+    final static public int PlaygroundWidth  = HorizontalGap*PointsX;
+
+
+    /**
+     * wysokosc boiska
+     */
+    final static public int PlaygroundHeight = VerticalGap*PointsY;
+
+
+    /**
+     * szerokosc calego apletu
+     */
+    final static public int WIDTH   = PlaygroundWidth+StartXGrass*2;
+
+
+    /**
+     * wysokosc apletu
+     */
+    final static public int HEIGHT  = PlaygroundHeight+StartYGrass*2;
+
+    
+    /**
+     * szerokosc bramki
+     */
+    final static public int GoalWidth  = 45;
+
+
+    /**
+     * wysokosc lini symulujacej bramke
+     */
+    final static public int GoalHeight = 5;
+
+
+    /**
+     * obszar podswietlany wokol aktualnego punktu w pionie
+     */
+    final static public int HoverAreaX = 30; 
+
+
+    /**
+     * obszar podswietlany wokol aktualnego punktu w poziomie
+     */
+    final static public int HoverAreaY = 30;
+
+    /**
+     * maksymalny dystans miedzy dwoma punktami, ktore mialy tworzy jeden ruch
+     */
     final static public double MaxDistance = Math.sqrt(Size.VerticalGap*Size.VerticalGap + Size.HorizontalGap*Size.HorizontalGap);
 
 
-        /**
-     * srodek boiska w poziomie
-     */
+   /**
+    * srodek boiska w poziomie
+    */
     public final static int xCenter = (int)(PlaygroundWidth/2) + StartXGrass;
 
 
@@ -70,13 +162,23 @@ public class Size {
      */
     public final static int yStop  = PlaygroundHeight + yStart;
 
+
+   /**
+    * wspolrzedna x na ktorej zaczynas sie bramka
+    */
     public final static int GoalXStart = (int)((PointsX / 2 - 1) *
                                                         HorizontalGap +
                                                         StartXGrass) - OffsetX;
 
-    public final static int GoalYBottom = PlaygroundHeight + StartYGrass;
-    
-    public final static int GoalYTop = StartYGrass - GoalHeight;
 
-    
+   /**
+    * wspolrzedna y na ktorej zaczynas sie bramka dolna
+    */
+    public final static int GoalYBottom = PlaygroundHeight + StartYGrass;
+
+
+   /**
+    * wspolrzedna y na ktorej zaczynas sie bramka gorna
+    */
+    public final static int GoalYTop = StartYGrass - GoalHeight;  
 }
